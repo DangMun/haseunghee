9c7024cddfeed2313e4007df3f7a758f9992910b
clo-lpT <hama4342@naver.com>
2022-06-05 21:30:55 +0900

    2022.06.05 add datepicker

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    
-  </div>
+   <Datepicker v-model="date"></Datepicker>
 </template>
 
 <script>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 export default {
   name: 'DatePicker',
   props: {
     msg: String
-  }
+  },
+  components: { Datepicker },
+        data() {
+            return {
+                date: null,
+            };
+        }
 }
 </script>
 <style>
